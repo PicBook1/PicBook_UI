f19ef093556e84ab8d4370bc13a6db5e17b6b756
fasil251 <51713628+fasil251@users.noreply.github.com>
2020-01-08 01:09:51 -1200

    Update server.go

--- server.go
+++ server.go
@@ -15,11 +15,16 @@ func index(w http.ResponseWriter, r *http.Request){
 func fav(w http.ResponseWriter, r *http.Request){
   templ.ExecuteTemplate(w,"fav.layout",nil)
  }
+func about(w http.ResponseWriter, r *http.Request){
+  templ.ExecuteTemplate(w,"about.layout",nil)
+ }
+
 func main() {
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("assets"))
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	http.HandleFunc("/", index)
-	http.HandleFunc("/fav",fav)
+	http.HandleFunc("/fav", fav)
+	http.HandleFunc("/about", about)
    	http.ListenAndServe(":8088", nil)
 }
