f1e14e0632f5c51131f4788e9c245482f102374a
gerizangeru <50832367+gerizangeru@users.noreply.github.com>
2020-01-10 13:36:19 +0300

    Update server.go

--- server.go
+++ server.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"html/template"
+	 "net/http"	 
+ )
+
+ var templ *template.Template
+ func init(){
+ 	templ = template.Must(template.ParseGlob("assets/templates/*"))
+}
+func index(w http.ResponseWriter, r *http.Request){
+	templ.ExecuteTemplate(w,"index.layout",nil)
+}
